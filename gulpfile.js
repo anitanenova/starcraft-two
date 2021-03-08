e018e1b07858d1fb3e27fec679b7f4ad669a407a
Ani <anita.nenova8@gmail.com>
2021-03-08 16:26:18 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
+
+gulp.task("sass", function () {
+  return gulp
+    .src("assets/styles/scss/styles.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("assets/styles/css"));
+});
